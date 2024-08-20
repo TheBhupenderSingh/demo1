d412a86af5a9536b1bb839095f93a3ec3c88ce4f
Bhupender Singh <141838379+TheBhupenderSingh@users.noreply.github.com>
2024-08-20 15:14:52 +0530

    Update Restc.java

--- src/main/java/com/example/demo/Restc.java
+++ src/main/java/com/example/demo/Restc.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class Restc {
 	@RequestMapping("/test")
 	public String getAllTo(){
-		return "Balle Balle";
+		return "Balle Balle12";
 
 }
 }
